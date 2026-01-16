88ee8fc49e1aeb852eae3a8f1a90727cadb520f4
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2026-01-16 22:57:35 +0100

    #update product class

--- Chapter10/Northwind.EntityModels/Product.cs
+++ Chapter10/Northwind.EntityModels/Product.cs
@@ -1,5 +1,26 @@
-﻿namespace Northwind.EntityModels;
+﻿using System.ComponentModel.DataAnnotations.Schema; // To use [Column]
+using System.ComponentModel.DataAnnotations; // To use [Required]
 
-    internal class Product
-    {
-    }
+namespace Northwind.EntityModels;
+
+internal class Product
+{
+    public int ProductId { get; set; } // Primary key
+
+    [Required]
+    [StringLength(40)] // Max length of 40 characters
+    public string ProductName { get; set; } = null!; // Not null
+
+    // Property name is different from the column name
+    [Column("UnitePrice", TypeName = "money")]
+    public decimal? Cost { get; set; }
+
+    [Column("UnitsInStock")]
+    public short? Stock { get; set; }
+
+    public bool Discontinued { get; set; }
+
+    // Foreign key property 
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; } = null!; // Navigation property to related Category
+}
