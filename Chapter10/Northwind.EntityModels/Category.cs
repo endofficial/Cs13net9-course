f83d7555e0abfe196b6590d4e58f2e7c2bfc43a9
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2026-01-16 22:42:58 +0100

    #update category class

--- Chapter10/Northwind.EntityModels/Category.cs
+++ Chapter10/Northwind.EntityModels/Category.cs
@@ -1,5 +1,16 @@
-﻿namespace Northwind.EntityModels;
-    public class Category
-    {
+﻿using System.ComponentModel.DataAnnotations.Schema; // To use [Column]
 
-    }
+namespace Northwind.EntityModels;
+
+public class Category
+{
+    // These properties map to columns in the Categories table
+    public int CategoryId { get; set; } // Primary key
+    public string CategoryName { get; set; } = null!; // Not null
+
+    [Column(TypeName = "ntext")]
+    public string? Description { get; set; } // Nullable
+
+    // Defines a navigation property for related rows.
+    public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>(); // ogni volta che hai una Categoria, puoi facilmente raggiungere tutti i Prodotti collegati ad essa.
+}
