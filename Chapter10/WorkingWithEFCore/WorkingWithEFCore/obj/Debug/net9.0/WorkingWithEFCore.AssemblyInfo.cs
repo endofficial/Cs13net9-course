80d1157708d5db5760aaac19b5946422bf20a753
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2026-01-20 12:16:07 +0100

    #update

--- Chapter10/WorkingWithEFCore/WorkingWithEFCore/obj/Debug/net9.0/WorkingWithEFCore.AssemblyInfo.cs
+++ Chapter10/WorkingWithEFCore/WorkingWithEFCore/obj/Debug/net9.0/WorkingWithEFCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkingWithEFCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e244f630dcfed826615668f10cdd6aaa732cce8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb436a2b7ca85fe40ec2dc1436eec325f97c873c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkingWithEFCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkingWithEFCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
